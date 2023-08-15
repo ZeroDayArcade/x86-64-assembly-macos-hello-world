b800795ac93f8ce0d0290f291a9bbdc98b8fe35e
zerodayarcade.com <141867962+ZeroDayArcade@users.noreply.github.com>
2023-08-15 03:20:27 -0700

    Text to match title, cooler num

--- hello_assembly.s
+++ hello_assembly.s
@@ -2,17 +2,17 @@
 .intel_syntax noprefix
 
 start:
-    # Write "Hello, World!"
+    # Write "Hello World"
     mov rax, 0x2000004                # system call 4 (write code)
     mov rdi, 1
     lea rsi, hello_world[rip]
-    mov rdx, 14                       # Set register rdx to 14
+    mov rdx, 12                       # Set register rdx to 12
     syscall                           # envoke syscall
 
     # Exit program
     mov rax, 0x2000001                # system call 1 (exit code)
-    mov rdi, 88                       # set the exit code to 88
+    mov rdi, 99                       # set the exit code to 99
     syscall
 
 hello_world:                          # Definition of hello_world
-    .asciz "Hello, World!\n"
+    .asciz "Hello World\n"
